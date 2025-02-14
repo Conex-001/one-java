5a2a68329d3e1237c66949f02dd17ea66dbf8d02
Conex-001 <Confidencevitee@gmail.com>
2025-02-14 04:13:49 -0800

    Updated index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,12 @@
-
 let score =100;
 if (score >=90 && score<=100){
     console.log("A");
-}
+}else if (score>= 80 && score<=89){
+    console.log("B");
+}else  if (score>= 70 && score<=79){
+    console.log("C");
+}else if (score>= 60 && score<=69){
+    console.log("D");
+}else if (score>= 50 && score<=59){
+    console.log("E");
+}else console.log("F");
